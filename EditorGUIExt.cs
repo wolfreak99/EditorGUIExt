7748445b6b69b3c786b5626d2fcea7c6e14d18a1
wolfreak99 <jbminor1991@gmail.com>
2016-02-24 12:01:27 -0500

    Fix alignment problem

--- EditorGUIExt.cs
+++ EditorGUIExt.cs
@@ -6,97 +6,88 @@ using System.Collections.Generic;
 
 namespace UnityEditorExtensions
 {
-    public interface IListItem
-    {
-        void DrawItem();
-        void DrawSelected();
-        Rect GetBounds();
-    }
+	public interface IListItem
+	{
+		void DrawItem();
+		void DrawSelected();
+		Rect GetBounds();
+	}
 
-    /** A standard list item with a label and an optional icon.
-     * 
-     */
-    public class LabelListItem : IListItem
-    {
-    	public LabelListItem(string name) { this.name = name; }
-        public string name;
-        public Func<Texture2D> IconFunction;
-        public float height = 16f;
-        public Color itemColor = Color.gray;
-        private Rect mItemRect;
-        public virtual void DrawItem()
-        {
-            Color tmp = GUI.color;
-            GUI.color = itemColor;
-            Texture2D icon = null;
-            if (IconFunction != null)
-                icon = IconFunction();
-            GUILayout.Label(new GUIContent(name, icon), GUILayout.MaxHeight(height));
-            if (Event.current.type == EventType.Repaint)
-            {
-                mItemRect = GUILayoutUtility.GetLastRect();
-            }
-            GUI.color = tmp;
-        }
+	/** A standard list item with a label and an optional icon.
+	 * 
+	 */
+	public class LabelListItem : IListItem
+	{
+		public LabelListItem(string name) { this.name = name; }
+		public string name;
+		public Func<Texture2D> IconFunction;
+		public float height = 16f;
+		public Color itemColor = Color.gray;
+		private Rect mItemRect;
+		public virtual void DrawItem() {
+			Color tmp = GUI.color;
+			GUI.color = itemColor;
+			Texture2D icon = null;
+			if (IconFunction != null)
+				icon = IconFunction();
+			GUILayout.Label(new GUIContent(name, icon), GUILayout.MaxHeight(height));
+			if (Event.current.type == EventType.Repaint) {
+				mItemRect = GUILayoutUtility.GetLastRect();
+			}
+			GUI.color = tmp;
+		}
 
-        public virtual void DrawSelected()
-        {
-            Color tmp = GUI.color;
-            GUI.color = Color.white;
-            GUI.Box(mItemRect, GUIContent.none);
-            DrawItem();
-            GUI.color = tmp;
-        }
+		public virtual void DrawSelected() {
+			Color tmp = GUI.color;
+			GUI.color = Color.white;
+			GUI.Box(mItemRect, GUIContent.none);
+			DrawItem();
+			GUI.color = tmp;
+		}
 
-        public Rect GetBounds()
-        {
-            return mItemRect;
-        }
-    }
+		public Rect GetBounds() {
+			return mItemRect;
+		}
+	}
 
-    /** A data structure to organize ListItems within an ItemList
-     * 
-     */
-    public class ListItemCollection : IEnumerable<IListItem>, IList<IListItem>
-    {
-        public ListItemCollection()
-        {
-            mItems = new List<IListItem>();
-            mSelected = new List<IListItem>();
-        }
+	/** A data structure to organize ListItems within an ItemList
+	 * 
+	 */
+	public class ListItemCollection : IEnumerable<IListItem>, IList<IListItem>
+	{
+		public ListItemCollection() {
+			mItems = new List<IListItem>();
+			mSelected = new List<IListItem>();
+		}
 
-        private List<IListItem> mItems;
-        private List<IListItem> mSelected;
-        public List<IListItem> Selected { get { return mSelected; } }
+		private List<IListItem> mItems;
+		private List<IListItem> mSelected;
+		public List<IListItem> Selected { get { return mSelected; } }
 
-        public IEnumerator<IListItem> GetEnumerator()
-        {
-            foreach (IListItem i in mItems)
-            {
-                if (i == null)
-                {
-                    break;
-                }
-                yield return i;
-            }    
-        }
+		public IEnumerator<IListItem> GetEnumerator() {
+			foreach (IListItem i in mItems) {
+				if (i == null) {
+					break;
+				}
+				yield return i;
+			}
+		}
 		IEnumerator IEnumerable.GetEnumerator() {
 			return GetEnumerator();
 		}
 
-        public void Add(IListItem item) { mItems.Add(item); }
-        public bool Remove(IListItem item) { return mItems.Remove(item); }
-        public void Clear() { mItems.Clear(); }
-        public int Count { get { return mItems.Count; } }
-        public bool ItemSelected(IListItem item) { return mSelected.Contains(item); }
+		public void Add(IListItem item) { mItems.Add(item); }
+		public bool Remove(IListItem item) { return mItems.Remove(item); }
+		public void Clear() { mItems.Clear(); }
+		public int Count { get { return mItems.Count; } }
+		public bool ItemSelected(IListItem item) { return mSelected.Contains(item); }
 		public bool ItemSelected(int index) { return ItemSelected(mItems[index]); }
-		public IListItem this[int i]
-        {
-            get { return mItems[i]; }
-            set { mItems[i] = value; }
-        }
+		public IListItem this[int i] {
+			get { return mItems[i]; }
+			set { mItems[i] = value; }
+		}
 
-        public void Select(IListItem item) {
+		public void Select(IListItem item) {
 			if (!mSelected.Contains(item)) {
 				mSelected.Add(item);
 			}
@@ -109,8 +100,8 @@ namespace UnityEditorExtensions
 		public void Select(int index) { Select(mItems[index]); }
 		public void Deselect(int index) { Deselect(mItems[index]); }
 
-        public void ClearSelection() { mSelected.Clear(); }
-        public int IndexOf(IListItem item) { return mItems.IndexOf(item); }
+		public void ClearSelection() { mSelected.Clear(); }
+		public int IndexOf(IListItem item) { return mItems.IndexOf(item); }
 		public void Insert(int index, IListItem item) { mItems.Insert(index, item); }
 		public void RemoveAt(int index) { mItems.RemoveAt(index); }
 		public bool Contains(IListItem item) { return mItems.Contains(item); }
@@ -118,14 +109,14 @@ namespace UnityEditorExtensions
 		public bool IsReadOnly {
 			get { return false; }
 		}
-    }
+	}
 
-    /** Contains all static GUI extention functions.
-     * 
-     */
-    public class EditorGUIExt
-    {
-        public static GUIStyle sDefBGStyle = GUIStyle.none;
+	/** Contains all static GUI extention functions.
+	 * 
+	 */
+	public class EditorGUIExt
+	{
+		public static GUIStyle sDefBGStyle = GUIStyle.none;
 
 		public static void ItemList(ListItemCollection items, ref Vector2 scrollPos, ref bool guiChanged, GUIStyle backgroundStyle, params GUILayoutOption[] options) {
 			ItemList(items, ref scrollPos, ref guiChanged, null, backgroundStyle, options);
@@ -136,7 +127,7 @@ namespace UnityEditorExtensions
 		public static void ItemList(ListItemCollection items, ref Vector2 scrollPos, ref bool guiChanged, params GUILayoutOption[] options) {
 			ItemList(items, ref scrollPos, ref guiChanged, null, sDefBGStyle, options);
 		}
-		
+
 		public static bool ItemList(ListItemCollection items, ref Vector2 scrollPos, Action updateContentsFunction, GUIStyle backgroundStyle, params GUILayoutOption[] options) {
 			bool guiChanged = false;
 			ItemList(items, ref scrollPos, ref guiChanged, updateContentsFunction, backgroundStyle, options);
@@ -197,10 +188,10 @@ namespace UnityEditorExtensions
 					}
 				}
 			}
-			
+
 			// Draw the ListView
 			if (options == null || options.Length == 0) {
-				options = new GUILayoutOption[]{ GUILayout.ExpandHeight(true) };
+				options = new GUILayoutOption[] { GUILayout.ExpandHeight(true) };
 			}
 			scrollPos = EditorGUILayout.BeginScrollView(scrollPos, bgStyle, options);
 			if (items != null) {
@@ -215,5 +206,5 @@ namespace UnityEditorExtensions
 			}
 			EditorGUILayout.EndScrollView();
 		}
-    }
+	}
 }
